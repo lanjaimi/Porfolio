a2b04ba2f8b27cf48d1e1d5c82442158764729a8
Lahcen Anjaimi <admin@MacBook.local>
2020-07-24 10:13:33 -0400

    added photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import headshot from '../images/headshot.jpg';
-import me from '../images/me.jpg';
+import me from '../mypics/head1.jpg';
 
 import { typingText } from '../Data.js';
 
@@ -9,10 +8,72 @@ export default class Home extends Component {
     super();
     this.state = {
       main: '',
+      secondary: '',
       forward: true,
+      speed: 300,
     };
   }
 
+  doubletyping = () => {
+    let arr = [
+      ['my name is', ['Lahcen']],
+      ["it's pronounced ", ['la-sin']],
+      [
+        "I'm a",
+        [
+          'Computer science student at BU',
+          'full stack developer',
+          'home chef',
+          'beer enthusiast',
+        ],
+      ],
+    ];
+
+    console.log(arr[0][1][1]);
+
+    this.typingEffects(arr);
+  };
+
+  typingEffects = (text) => {
+    console.log('called');
+    let k = 0;
+    let i = 0;
+    let j = 1;
+
+    setInterval(() => {
+      this.setState({
+        main: text[k][0],
+      });
+      this.setState({
+        secondary: text[k][1][i].substring(0, j),
+      });
+
+      if (this.state.forward) j++;
+      if (j > text[k][1][i].length)
+        this.setState({
+          forward: false,
+        });
+
+      if (!this.state.forward) {
+        j--;
+      }
+
+      if (j === -1) {
+        j = 1;
+        i++;
+        this.setState({
+          secondary: '',
+          forward: true,
+        });
+      }
+      if (i === text[k][1].length) {
+        i = 0;
+        k++;
+      }
+      if (k === text.length) k = 0;
+    }, 200);
+  };
+  /* 
   typingEffects = (text) => {
     let i = 0;
     let j = 1;
@@ -42,10 +103,12 @@ export default class Home extends Component {
       }
       if (i === text.length) i = 0;
     }, 500);
-  };
+  }; */
 
   componentDidMount() {
-    this.typingEffects(typingText);
+    //  this.typingEffects(typingText);
+
+    this.doubletyping();
   }
 
   render() {
@@ -55,7 +118,8 @@ export default class Home extends Component {
           <img className='my_pic' src={me} alt='mu picture' />
           <div className='box'>
             <h1>
-              programming is <span>{this.state.main}</span>
+              {this.state.main + ' '}
+              <span>{this.state.secondary}</span>
             </h1>
           </div>
           <div className='text'>
